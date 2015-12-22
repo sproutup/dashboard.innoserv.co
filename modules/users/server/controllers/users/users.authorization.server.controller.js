03992c511a749f6d2a1ad0da804bd4a4d231046a
Peter Andersen <peter@ryanandersen.dk>
2015-12-22 16:00:39 -0500

    bugfix: fix issue with user controller

--- modules/users/server/controllers/users/users.authorization.server.controller.js
+++ modules/users/server/controllers/users/users.authorization.server.controller.js
@@ -11,22 +11,17 @@ var _ = require('lodash'),
  * User middleware
  */
 exports.userByID = function (req, res, next, id) {
-  if (!dynamoose.Types.ObjectId.isValid(id)) {
+  if (!_.isString(id)) {
     return res.status(400).send({
       message: 'User is invalid'
     });
   }
 
-  User.findOne({
-    _id: id
-  }).exec(function (err, user) {
-    if (err) {
-      return next(err);
-    } else if (!user) {
-      return next(new Error('Failed to load User ' + id));
-    }
-
-    req.profile = user;
+  User.get(id).then(function(item){
+    req.profile = item;
     next();
+  })
+  .catch(function (err) {
+    return next(err);
   });
 };
