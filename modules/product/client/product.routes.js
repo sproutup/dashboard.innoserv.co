ee2f51da04cef0694ed0fe1c03e68aa1b8ef9d36
Tao Ni <tao.ni.81@gmail.com>
2015-12-23 16:40:44 -0500

    product mock routes

--- modules/product/client/product.routes.js
+++ modules/product/client/product.routes.js
@@ -8,13 +8,23 @@ angular.module('product').config(['$stateProvider',
         abstract: true,
         template: '<div ui-view></div>'
       })
-      .state('user.product.dashboard', {
+      // product list and mock
+      .state('user.product.list', {
         url: '/:companyId',
         templateUrl: 'modules/product/client/product.html'
       })
-      .state('user.product.mock', {
+      .state('user.product.list-mock', {
         url: '/:companyId/mock',
         templateUrl: 'modules/product/client/product-mock.html'
+      })
+      //product create and mock
+      .state('user.product.create', {
+        url: '/create',
+        templateUrl: 'modules/product/client/product-create.html'
+      })
+      .state('user.product.create-mock', {
+        url: '/create',
+        templateUrl: 'modules/product/client/product-create-mock.html'
       });
   }
 ]);
