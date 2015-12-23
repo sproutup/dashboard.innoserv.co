bd54cac865beced294fab40c513f9bb2913558cd
Thomas O'Brien <tbobrien612@gmail.com>
2015-12-22 20:19:11 -0500

    Routes fix

--- modules/product/client/product.routes.js
+++ modules/product/client/product.routes.js
@@ -6,16 +6,14 @@ angular.module('product').config(['$stateProvider',
       .state('user.product', {
         url: '/product',
         abstract: true,
-        template: '<div ui-view></div>',
-        controller: 'productController',
-        controllerAs: 'vm'
+        template: '<div ui-view></div>'
       })
       .state('user.product.dashboard', {
         url: '/:companyId',
         templateUrl: 'modules/product/client/product.html'
       })
       .state('user.product.mock', {
-        url: '/mock',
+        url: '/:companyId/mock',
         templateUrl: 'modules/product/client/product-mock.html'
       });
   }
