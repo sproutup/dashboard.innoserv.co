f7e3e5fa42721564e31236413003492a2bbdb1fa
Peter Andersen <peter@ryanandersen.dk>
2015-12-29 14:07:04 -0500

    added company slug. /api/company/slug/[slug] get/put/delete

--- modules/company/server/company.routes.js
+++ modules/company/server/company.routes.js
@@ -18,6 +18,13 @@ module.exports = function (app) {
     .put(companies.update)
     .delete(companies.delete);
 
+  // Single article routes
+  app.route('/api/company/slug/:companySlug').all(companyPolicy.isAllowed)
+    .get(companies.read)
+    .put(companies.update)
+    .delete(companies.delete);
+
   // Finish by binding the company middleware
   app.param('companyId', companies.companyByID);
+  app.param('companySlug', companies.companyBySlug);
 };
