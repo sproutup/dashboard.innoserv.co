3bba2d5e298a902c92cd50b4b11f9c935df275b1
Peter Andersen <peter@ryanandersen.dk>
2015-12-28 19:40:04 -0500

    migrate admin user interface

--- modules/users/client/services/users.client.service.js
+++ modules/users/client/services/users.client.service.js
@@ -17,7 +17,7 @@ angular.module('users').factory('Users', ['$resource',
 angular.module('users.admin').factory('Admin', ['$resource',
   function ($resource) {
     return $resource('api/users/:userId', {
-      userId: '@_id'
+      userId: '@id'
     }, {
       update: {
         method: 'PUT'
