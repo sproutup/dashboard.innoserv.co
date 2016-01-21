8e17816a623d8f06b76c154442d197bf76e66f39
Thomas O'Brien <tbobrien612@gmail.com>
2016-01-21 16:26:53 -0500

    Delete modal added to campaigns and templates

--- modules/campaign/client/campaign.controller.js
+++ modules/campaign/client/campaign.controller.js
@@ -6,9 +6,9 @@
       .module('campaign')
       .controller('CampaignController', CampaignController);
 
-    CampaignController.$inject = ['$scope', '$rootScope', '$state', 'CampaignService', '$location', 'Authentication', 'Menus', 'ProductService'];
+    CampaignController.$inject = ['$scope', '$rootScope', '$state', 'CampaignService', '$location', 'Authentication', 'Menus', 'ProductService', '$modal'];
 
-    function CampaignController($scope, $rootScope, $state, CampaignService, $location, Authentication, Menus, ProductService) {
+    function CampaignController($scope, $rootScope, $state, CampaignService, $location, Authentication, Menus, ProductService, $modal) {
       var vm = this;
       vm.create = create;
       vm.initTemplate = initTemplate;
@@ -17,6 +17,7 @@
       vm.cancel = cancel;
       vm.find = find;
       vm.findOne = findOne;
+      vm.openModal = openModal;
       vm.socialOptions = [
         {  title: 'Twitter',
            type: 'tw' },
@@ -225,6 +226,23 @@
         //     console.log(err);
         //   });
         // });
+      }
+
+      function openModal(item) {
+        var modalInstance = $modal.open({
+          templateUrl: 'modules/core/client/delete-confirmation.html',
+          controller: 'DeleteController',
+          controllerAs: 'vm',
+          resolve: {
+            message: function() { return 'Deleting a campaign is risky business.'; }
+          }
+        });
+
+        modalInstance.result.then(function () {
+          remove(item);
+        }, function () {
+          console.log('Modal dismissed at: ' + new Date());
+        });
       }
 
 //       findProducts();
