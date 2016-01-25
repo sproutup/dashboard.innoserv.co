bb5546913d0fcd1811a25a2d6ed86a329350f4cc
Peter Andersen <peter@ryanandersen.dk>
2016-01-25 09:26:30 -0500

    bugfix: fixed general signup confirmation page when new company

--- modules/landing/client/landing.routes.js
+++ modules/landing/client/landing.routes.js
@@ -11,9 +11,20 @@ angular.module('landing').config(['$stateProvider',
         controller: 'landingController',
         controllerAs: 'vm'
       })
+      .state('landing', {
+        url: '/landing',
+        abstract: true,
+        template: '<div ui-view></div>',
+        controller: 'landingController',
+        controllerAs: 'vm'
+      })
       .state('company.landing.default', {
         url: '',
         templateUrl: 'modules/landing/client/landing.view.html'
+      })
+      .state('landing.confirmation', {
+        url: '/confirmation',
+        templateUrl: 'modules/landing/client/landing-confirmation.view.html'
       })
       .state('company.landing.confirmation', {
         url: '/confirmation',
