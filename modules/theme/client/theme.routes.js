49ab57e08e58272684d2cc8e4790af37f047da68
Tao Ni <tao.ni.81@gmail.com>
2016-01-25 17:15:34 -0500

    mock - add trial campaign stats placeholder page

--- modules/theme/client/theme.routes.js
+++ modules/theme/client/theme.routes.js
@@ -55,6 +55,10 @@ angular.module('theme').config(['$stateProvider',
         url: '/view-trial-requests',
         templateUrl: 'modules/theme/client/mocks/view-trial-requests.view.html'
       })
+      .state('theme.mocks.view-trial-stats', {
+        url: '/view-trial-stats',
+        templateUrl: 'modules/theme/client/mocks/view-trial-stats.view.html'
+      })
       .state('theme.mocks.trial-request-details', {
         url: '/trial-request-details',
         templateUrl: 'modules/theme/client/mocks/trial-request-details.view.html'
