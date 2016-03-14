2b1dac648a61c47738b6e8aba71e70657fdab0f4
Thomas O'Brien <tbobrien612@gmail.com>
2016-03-14 16:49:24 -0400

    Landing routes fix

--- modules/landing/client/landing.routes.js
+++ modules/landing/client/landing.routes.js
@@ -4,6 +4,17 @@
 angular.module('landing').config(['$stateProvider',
   function ($stateProvider) {
     $stateProvider
+      .state('footer.landing', {
+        url: '/landing',
+        abstract: true,
+        template: '<div ui-view></div>',
+        controller: 'landingController',
+        controllerAs: 'vm'
+      })
+      .state('footer.landing.confirmation', {
+        url: '/confirmation?url',
+        templateUrl: 'modules/landing/client/landing-confirmation.view.html'
+      })
       .state('footer.company', {
         url: '/landing/:companySlug',
         abstract: true,
@@ -18,20 +29,9 @@ angular.module('landing').config(['$stateProvider',
         controller: 'landingController',
         controllerAs: 'vm'
       })
-      .state('footer.landing', {
-        url: '/landing',
-        abstract: true,
-        template: '<div ui-view></div>',
-        controller: 'landingController',
-        controllerAs: 'vm'
-      })
       .state('footer.company.landing.default', {
         url: '',
         templateUrl: 'modules/landing/client/landing.view.html'
-      })
-      .state('footer.landing.confirmation', {
-        url: '/confirmation?url',
-        templateUrl: 'modules/landing/client/landing-confirmation.view.html'
       })
       .state('footer.company.landing.confirmation', {
         url: '/confirmation?url',
